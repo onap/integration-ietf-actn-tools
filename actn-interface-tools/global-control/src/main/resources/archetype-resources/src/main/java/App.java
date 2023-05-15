aeb5444e80bc8d9a6f33a43b106b7d4736fc0e81
ruilin cai <ruilin.cai@huawei.com>
2023-05-15 14:51:41 -0400

    created restconf module, added functions of restful calling methods. Also created empty shell for global-control and yang-utils
    Change-Id: Idfc04649285bd6c5b67b1cc6c2115bf4bb267660
    Signed-off-by: ruilin cai <ruilin.cai@huawei.com>
    Issue-ID: INT-2213

---
+++ actn-interface-tools/global-control/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.onap.integration.ietf-actn-tools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
