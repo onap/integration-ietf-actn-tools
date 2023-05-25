3d61a9aadd481b26c7e86d6f3cd012ccdb87063a
ruilin cai <ruilin.cai@huawei.com>
2023-05-25 13:02:58 -0400

    Added global-app module.
    Change-Id: I2032b9485a0b625193cb0b22f960d4f8739efa77
    Signed-off-by: ruilin cai <ruilin.cai@huawei.com>
    Issue-ID: INT-2213

---
+++ actn-interface-tools/global-control/app/src/main/java/org/onap/integration/actninterfacetools/globalapp/YangManagerUtils.java
@@ -0,0 +1,99 @@
+package org.onap.integration.actninterfacetools.globalapp;
+
+import org.onosproject.yang.compiler.tool.YangCompilerManager;
+import org.onosproject.yang.compiler.tool.YangNodeInfo;
+import org.onosproject.yang.model.YangModel;
+import org.onosproject.yang.runtime.helperutils.YangApacheUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class YangManagerUtils {
+    private static final String SLASH;
+    private static final String HYPHEN = "-";
+    private static final String PERIOD = ".";
+    private static final String YANG_RESOURCES = "yang/resources";
+    private static final String SYSTEM;
+    private static final String MAVEN = "mvn:";
+    private static final String JAR = ".jar";
+    private static final String USER_DIRECTORY = "user.dir";
+    private static final Logger log;
+
+    private YangManagerUtils() {
+    }
+    public static YangModel getYangModel(Class<?> modClass) throws URISyntaxException {
+        String jarPath = new File(YangManagerUtils.class.getProtectionDomain().getCodeSource().getLocation().toURI()).getPath();
+        String yangModelPath = jarPath.replaceAll("/global-control/app/target/classes", "/actn-model/target/actn-model-1.0-SNAPSHOT");
+        List<YangNodeInfo> nodeInfo = new ArrayList();
+//        System.out.println(yangModelPath);
+        String metaPath = yangModelPath + SLASH + "yang/resources" + SLASH;
+        YangModel model = processJarParsingOperations(yangModelPath);
+        if (model != null) {
+            YangCompilerManager.setNodeInfo(model, nodeInfo);
+            if (!nodeInfo.isEmpty()) {
+                return YangCompilerManager.processYangModel(metaPath, nodeInfo, model.getYangModelId(), false);
+            }
+        }
+
+        return null;
+    }
+    private static String getJarPathFromBundleLocation(String mvnLocationPath, String currentDirectory) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(currentDirectory).append(SYSTEM);
+        StringBuilder ver = new StringBuilder();
+        if (mvnLocationPath.contains("mvn:")) {
+            String[] strArray = mvnLocationPath.replaceFirst("\\$Bundle-.*$", "").split("mvn:");
+            if (strArray[1].contains(File.separator)) {
+                String[] split = strArray[1].split(File.separator);
+                if (split[0].contains(".")) {
+                    String[] groupId = split[0].split(Pattern.quote("."));
+                    String[] var7 = groupId;
+                    int var8 = groupId.length;
+
+                    for(int var9 = 0; var9 < var8; ++var9) {
+                        String s = var7[var9];
+                        builder.append(s).append(SLASH);
+                    }
+
+                    for(int i = 1; i < split.length; ++i) {
+                        builder.append(split[i]).append(SLASH);
+                        ver.append(split[i]).append("-");
+                    }
+
+                    builder.append(ver);
+                    builder.deleteCharAt(builder.length() - 1);
+                    return builder.toString();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static YangModel processJarParsingOperations(String path) {
+        String jar = path + ".jar";
+
+        try {
+            File file = new File(jar);
+            if (file.exists()) {
+                return YangCompilerManager.parseJarFile(path + ".jar", path);
+            }
+        } catch (IOException var3) {
+            log.error(" failed to parse the jar file in path {} : {} ", path, var3.getMessage());
+        }
+
+        return null;
+    }
+
+    static {
+        SLASH = File.separator;
+        SYSTEM = SLASH + "system" + SLASH;
+        log = LoggerFactory.getLogger(YangApacheUtils.class);
+    }
+}
---
+++ actn-interface-tools/global-control/app/src/main/java/org/onap/integration/actninterfacetools/globalapp/impl/GlobalServiceImpl.java
@@ -0,0 +1,172 @@
+package org.onap.integration.actninterfacetools.globalapp.impl;
+
+import com.google.auto.service.AutoService;
+import org.onap.integration.actninterfacetools.actnclient.impl.ActnClientServiceImpl;
+import org.onap.integration.actninterfacetools.globalapi.ActnClientService;
+import org.onap.integration.actninterfacetools.globalapi.ActnDataConverter;
+import org.onap.integration.actninterfacetools.globalapi.GlobalService;
+import org.onap.integration.actninterfacetools.globalapp.YangClassLoaderRegistry;
+import org.onosproject.yang.model.*;
+import org.onosproject.yang.runtime.*;
+import org.onosproject.yang.runtime.impl.*;
+
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+@AutoService(GlobalService.class)
+public class GlobalServiceImpl implements GlobalService, YangModelRegistry,
+        YangSerializerRegistry, YangRuntimeService, ModelConverter,
+        SchemaContextProvider, YangClassLoaderRegistry {
+    private static volatile GlobalServiceImpl globalService = null;
+      private static GlobalServiceUtil globalServiceUtil= GlobalServiceUtil.getNewGlobalService();
+
+    public GlobalServiceImpl() {
+    }
+    public static GlobalServiceImpl getNewGlobalService(){
+        if(globalService != null){
+            return globalService;
+        }
+        synchronized (ActnClientServiceImpl.class) {
+            if(globalService == null){
+                globalService = new GlobalServiceImpl();
+
+            }
+            return globalService;
+        }
+    }
+
+    @Override
+    public String getServiceName() {
+        return "GlobalServiceImpl";
+    }
+
+    @Override
+    public void initialize() throws URISyntaxException {
+        globalServiceUtil.initialize();
+    }
+
+    @Override
+    public ActnClientService getActnClientService(ActnDataConverter actnDataConverter) throws URISyntaxException {
+        return globalServiceUtil.getActnClientService(actnDataConverter);
+    }
+
+    public DefaultYangModelRegistry getModelRegistry(){
+
+        return globalServiceUtil.getModelRegistry();
+    }
+    public DefaultYangSerializerRegistry getSerializerRegistry(){
+        return globalServiceUtil.getSerializerRegistry();
+    }
+    public DefaultYangRuntimeHandler getRuntimeService(){
+        return globalServiceUtil.getRuntimeService();
+    }
+    public DefaultModelConverter getModelConverter(){
+        return globalServiceUtil.getModelConverter();
+    }
+    public DefaultSchemaContextProvider getSchemaContextProvider(){
+        return globalServiceUtil.getSchemaContextProvider();
+    }
+
+    public Map<String, ClassLoader> getClassLoaders(){
+        return globalServiceUtil.getClassLoaders();
+    }
+
+
+    @Override
+    public ClassLoader getClassLoader(String modelId) {
+        return globalServiceUtil.getClassLoader(modelId);
+    }
+
+    @Override
+    public void registerClassLoader(String modelId, ClassLoader classLoader) {
+        globalServiceUtil.registerClassLoader(modelId, classLoader);
+    }
+
+    @Override
+    public void unregisterClassLoader(String modelId) {
+        globalServiceUtil.unregisterClassLoader(modelId);
+    }
+
+    @Override
+    public ModelObjectData createModel(ResourceData resourceData) {
+        return globalServiceUtil.createModel(resourceData);
+    }
+
+    @Override
+    public ResourceData createDataNode(ModelObjectData modelObjectData) {
+        return globalServiceUtil.createDataNode(modelObjectData);
+    }
+
+    @Override
+    public SchemaContext getSchemaContext(ResourceId resourceId) {
+        return globalServiceUtil.getSchemaContext(resourceId);
+    }
+
+    @Override
+    public RpcContext getRpcContext(ResourceId resourceId) {
+        return globalServiceUtil.getRpcContext(resourceId);
+    }
+
+    @Override
+    public void registerModel(ModelRegistrationParam modelRegistrationParam) throws IllegalArgumentException {
+        globalServiceUtil.registerModel(modelRegistrationParam);
+    }
+
+
+    @Override
+    public void registerAnydataSchema(ModelObjectId parentModId, ModelObjectId childModId) throws IllegalArgumentException {
+        globalServiceUtil.registerAnydataSchema(parentModId, childModId);
+    }
+
+    @Override
+    public void unregisterAnydataSchema(Class parentClass, Class childClass) throws IllegalArgumentException {
+        globalServiceUtil.unregisterAnydataSchema(parentClass, childClass);
+    }
+
+    @Override
+    public void unregisterModel(ModelRegistrationParam modelRegistrationParam) {
+        globalServiceUtil.unregisterModel(modelRegistrationParam);
+    }
+
+    @Override
+    public Set<YangModel> getModels() {
+        return globalServiceUtil.getModels();
+    }
+
+    @Override
+    public YangModel getModel(String s) {
+        return globalServiceUtil.getModel(s);
+    }
+
+    @Override
+    public YangModule getModule(YangModuleId yangModuleId) {
+        return globalServiceUtil.getModule(yangModuleId);
+    }
+
+    @Override
+    public CompositeData decode(CompositeStream cs, RuntimeContext rc) {
+        return globalServiceUtil.decode(cs, rc);
+    }
+
+    @Override
+    public CompositeStream encode(CompositeData cd, RuntimeContext rc) {
+        return globalServiceUtil.encode(cd, rc);
+    }
+
+    @Override
+    public void registerSerializer(YangSerializer ys) {
+        globalServiceUtil.registerSerializer(ys);
+    }
+
+    @Override
+    public void unregisterSerializer(YangSerializer ys) {
+        globalServiceUtil.unregisterSerializer(ys);
+    }
+
+    @Override
+    public Set<YangSerializer> getSerializers() {
+        return globalServiceUtil.getSerializers();
+    }
+}
